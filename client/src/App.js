8859df9a46aa0aac156cc48b6942f88e23ad6140
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-07-03 19:32:35 -0400

    Complete conecting React front end with Flask backend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/members")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {typeof data.members === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => <p key={i}>{member}</p>)
+      )}
+    </div>
+  );
+}
+
+export default App;
